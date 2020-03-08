885de33763a8d526580806164add75bde737dc23
mantielero <josemaria.alkala@gmail.com>
2020-03-08 23:13:22 +0100

    Working with VapourSynth and GR

--- h2nim.py
+++ h2nim.py
@@ -34,8 +34,8 @@ def add_header_and_libname(headerFileName, libFileName):
     name = os.path.splitext(tmp)[0]
     # Macros
     tmp = []
-    tmp.append( (f"header{name}", f"\"{headerFileName}\"", True,1) )
-    tmp.append( (f"libName", f"\"{libFileName}\"", False,1) )
+    tmp.append( (f"header{name}", f"\"{headerFileName}\"", True,1, None) )
+    tmp.append( (f"libName", f"\"{libFileName}\"", False,1, None) )
 
     return {"const": tmp}
 
@@ -45,48 +45,48 @@ def parse_macros( data, parser ):
         # Better if we have the actual values
         val = parser.defs["values"][k]
         if val != None:
-            tmp.append( (k, val, False,1) )
+            tmp.append( (k, val, False,1, None) )
         else:
-            tmp.append( (k, '""', False,1) )
+            tmp.append( (k, '""', False,1, None) )
     _old = data.get("const", [])
-    data["const"] = _old + tmp     
+    data["const"] = _old + tmp + [(None, None, False, 0, None)]  
     return data
 
 # Enums
 def parse_enums( data, parser ):
-    _tmp = {"top" : [("import nimterop/types   # Provides \"defineEnum\"",None, True, 0)]}
-    
-    #enums = \n\n\n"
+    _d = {"top" : []}    
     enumlist = []
     enum_define = []
     tmp = []
+    _flag = True
     for k,v in parser.defs["enums"].items():
         enumlist.append(k)
-        _tmp["top"].append((f"defineEnum({k})", None, False,0))
-        _tmp["top"][2] = False # Don't comment
+        _d["top"].append((f"defineEnum({k})", None, False,0, None))
+        _flag = False # Don't comment
         for k1,v1 in v.items():
-            tmp.append( (f"{k1}", f"({v1}).{k}", False, 2) )
-        tmp.append( (None, None, False,0))  # Add a space
+            tmp.append( (f"{k1}", f"({v1}).{k}", False, 1, None) )
+        tmp.append( (None, None, False,0, None))  # Add a space
 
 
     for k, v in parser.defs["types"].items():
         if v.type_spec.startswith("enum "):
             if not k in enumlist:
-                _tmp["top"].append( f"defineEnum({k})" )
-                _tmp["top"][2] = False                
+                _d["top"].append( (f"defineEnum({k})", None, False, 0, None) )
+                _flag = False                
                 # This is a corner case for VapourSynth where pyclibrary doesn't parse properly an enum
                 _tmp = v.type_spec.split(f"enum {k}")[1].strip()
                 
                 _tmp = _tmp.split()
                 for i in range(len(_tmp)//2):
-                    tmp.append( (f"  {_tmp[i*2]}", "({_tmp[i*2+1]}).{k}", False) ) 
-                tmp.append( (None, None, False))  # Add a space    
+                    tmp.append( (f"{_tmp[i*2]}", f"({_tmp[i*2+1]}).{k}", False, 1, None) ) 
+                tmp.append( (None, None, False, 0, None))  # Add a space    
 
     _old = data.get("top", [])
-    data["top"] = _old + _tmp["top"]  
+    _tmp2 = [("import nimterop/types   # Provides \"defineEnum\"",None, _flag, 0, None), (None, None, False, 0, None)]
+    data["top"] = _old + _tmp2 + _d["top"] + [(None, None, False, 0, None)]
 
     _old = data.get("const", [])
-    data["const"] = _old + tmp           
+    data["const"] = _old + tmp        
     return data
 
 def create_pragma( name ):
@@ -240,7 +240,7 @@ def create_types(data, parser):
             if _new_name != None:
                 _object_name = _new_name
 
-        _list.append((f"{_object_name}", "object", False, 1) )  #importc: \"struct {k}\", header: header{name},
+        _list.append((f"{_object_name}", "object", False, 1, {"bycopy":"{.bycopy.}"}) )  #importc: \"struct {k}\", header: header{name},
 
 
 
@@ -256,7 +256,7 @@ def create_types(data, parser):
                 _type = get_return(member)
                 _new_type = convert_type(_type)
                 
-                _list.append((f"{_name}",f"{_new_type}", False, 2) )
+                _list.append((f"{_name}",f"{_new_type}", False, 2, None) )
 
             # Function signatures declarations
             else:
@@ -273,17 +273,17 @@ def create_types(data, parser):
                         _parameters += f'{t["name"]}:{_t}' 
                 
                 if ret_type == "void":
-                    _list.append( (f"{fname}", f"proc({_parameters}) {{.cdecl.}}", False, 2) )
+                    _list.append( (f"{fname}", f"proc({_parameters}) {{.cdecl.}}", False, 2, None) )
                 else:
-                    _list.append( (f"{fname}", f"proc({_parameters}):{ret_type} {{.cdecl.}}", False, 2) )
+                    _list.append( (f"{fname}", f"proc({_parameters}):{ret_type} {{.cdecl.}}", False, 2, None) )
 
 
-        _list.append((None, None, False, 0))
+        _list.append((None, None, False, 0, None))
     _old = data.get("type", [])
     data["type"] = _old +_list
     return data
 
-def gen_new_function(name, fname, params,ret_type):
+def gen_new_function(fname, params,ret_type,level = 1):
     new = "proc("
     add_comma = False
     _parameters = ""
@@ -300,20 +300,28 @@ def gen_new_function(name, fname, params,ret_type):
         new += f"{_parameters}) {{.cdecl.}}\n"
     else:
         new += f"{_parameters}):{ret_type} {{.cdecl.}}\n"    
-    _tmp = ("fname", new, False, 2 )
+    _tmp = (fname, new, False, level, None )
     return _tmp
 
 
-def create_something(data, parser):
+def get_function_signatures_types(data, parser):
+    """Creates types for function signatures::
+
+        type
+          VSGetVapourSynthAPI* = proc(version: cint):ptr VSAPI {.cdecl.}  
+
+    """
     _tmp = []
     for k, v in parser.defs["types"].items():
         #if v.is_fund_type() and not k.startswith("struct ") and not v.type_spec.startswith("struct ") and not k.startswith("enum ") and not v.type_spec.startswith("enum "):
         if not k.startswith("struct ") and not v.type_spec.startswith("struct ") and not k.startswith("enum ") and not v.type_spec.startswith("enum "):
-            fname = k
+            #print(k,v)
+            fname = k  
             ret = get_return2(v)
             params = get_parameters2(v)
             ret_type = convert_type(ret)
-            new = gen_new_function(name, fname, params,ret_type)
+            #print(ret_type)
+            new = gen_new_function( fname, params,ret_type)
             _tmp.append( new )
 
         #elif not k.startswith("struct ") and not v.type_spec.startswith("struct ") and not k.startswith("enum ") and not v.type_spec.startswith("enum "):
@@ -364,7 +372,7 @@ def create_functions(data, parser):
             _tmp += f"):{ret_type}" 
         #_tmp += f"    {{.importc,dynlib: libName.}}\n"
         #functions += _tmp
-        _lista.append( (_tmp, "", False, 0) )
+        _lista.append( (_tmp, "", False, 0, None) )
     
     _old = data.get("functions", [])
     data["functions"] = _old +_lista        
@@ -374,13 +382,16 @@ def create_functions(data, parser):
 def create_text(data, tab = 2):
     # Top + defineEnums
     _txt = ""
-    for txt1, txt2, comment, level in data["top"]:
+    for txt1, txt2, comment, level, _ in data["top"]:
         if not comment:
-            _txt += " "*level*tab + txt1 + "\n\n\n" 
+            if level == 0 and txt1 == None and txt2 == None:
+               _txt += "\n"
+            else:
+               _txt += " "*level*tab + txt1 + "\n" 
     
     # Consts
     _txt += "const\n"
-    for txt1, txt2, comment, level in data["const"]:
+    for txt1, txt2, comment, level,_ in data["const"]:
         spaces = level * tab * " "
         sep = "="
         if level > 1:
@@ -392,20 +403,25 @@ def create_text(data, tab = 2):
                 _txt += spaces +  f"{txt1}* {sep} {txt2}\n"
     # Types
     _txt += "type\n"
-    for txt1, txt2, comment, level in data["type"]:
+    for txt1, txt2, comment, level, d in data["type"]:
+        #print(txt1)
         spaces = level * tab * " "
         if not comment:
             if level == 0 and txt1 == None and txt2 == None:
                 _txt += "\n"
             elif level == 1:
                 # {.bycopy.} pass the structs by value
-                _txt += spaces +  f"{txt1}* {{.bycopy.}} = {txt2}\n"
+                _bycopy = ""
+                if d != None:
+                   _bycopy = d.get("bycopy", "")
+                
+                _txt += spaces +  f"{txt1}* {_bycopy} = {txt2}\n"
             elif level == 2:
                 _txt += spaces +  f"{txt1}* : {txt2}\n"
     
     # Functions
     _txt += "{.push dynlib:libName,importc.}\n"
-    for txt1, txt2, comment, level in data["functions"]:
+    for txt1, txt2, comment, level, _ in data["functions"]:
         if not comment:
             _txt += f"{txt1}\n"
     _txt += "{.pop.}\n"
@@ -427,10 +443,9 @@ if __name__ == "__main__":
     data = parse_macros(data, parser) 
     data = parse_enums( data, parser )
     data = create_types(data, parser)
-    data = create_something(data, parser) 
+    data = get_function_signatures_types(data, parser) 
     data = create_functions(data, parser)
     #-----
     txt = create_text(data)
     args.output.write(txt)
-    
     
